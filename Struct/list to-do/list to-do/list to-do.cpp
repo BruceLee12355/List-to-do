b11cecb2a2fede9aa8c4f60adcbae17652f0f06e
Ruslan <130584207+BruceLee12355@users.noreply.github.com>
2023-06-19 18:49:53 +0300

    add func editDelo

--- Struct/list to-do/list to-do/list to-do.cpp
+++ Struct/list to-do/list to-do/list to-do.cpp
@@ -1,27 +1,8 @@
 ﻿#include <iostream>
 #include <string>
+#include "func.h"
+
 using namespace std;
-
-
-struct delo
-{
-    string name;
-    int prior;
-    string ops;
-    int date;
-};
-
-
-void inputDelo(delo& d)
-{
-    setlocale(LC_ALL, "Russian");
-    cout << "Введите название задания: "; cin >> d.name;
-    cout << "Введите приоритет задания (от 1 до 10 где 1 наивысший приоритет!):"; cin >> d.prior;
-    cout << "Введите  описание задания: "; cin >> d.ops;
-    cout << "Введите  дату задания в формате день /неделя/месяц "; cin >> d.date;
-}
-
-
 
 int main()
 {
@@ -35,15 +16,18 @@ int main()
     {
         cout << "MENU" << endl;
         cout << "1 - Ввести новую задачу;" << endl;
-        cout << "2 - Удалить задачу;" << endl;
-        cout << "3 - редактировать задание;" << endl;
+        cout << "2 - Редактировать дело;" << endl;
+        cout << "3 - Удалить задачу;" << endl;
         cout << "4 - найти задание;" << endl;
         cout << "5 - вывести на экран задания;" << endl;
         cin >> menu;
         switch (menu) 
         {
         case 1:
-            inputDelo;
+            inputDelo();
+            break;
+        case 2:
+            editDelo();
             break;
         };
     }
